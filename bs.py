874448935a91239f103444c85e3e39fa14461a90
headrotor <rrmutt@gmail.com>
2021-05-14 11:12:19 -0700

    data

--- bs.py
+++ bs.py
@@ -3,27 +3,25 @@ from urllib.request import urlopen
 from datetime import datetime
 import time
 import sys
-url = "http://olympus.realpython.org/profiles/dionysus"
 url = "https://poweroutage.us/area/utility/380"
 
+#print("scraping {}".format(url))
+#sys.stdout.flush()
 
-print("scraping {}".format(url))
-sys.stdout.flush()
-while True:
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
 
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
+text = soup.get_text()
 
-    text = soup.get_text()
+for line in text.split("\n"):
+    if len(line.strip()) > 0:
+        words = line.split()
+        if len(words) == 3:
+            if words[1] == 'Outages:':
 
-    for line in text.split("\n"):
-        if len(line.strip()) > 0:
-            words = line.split()
-            if len(words) == 3:
-                if words[1] == 'Outages:':
+                print("{} {} {}".format(time.time(),
+                                        datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                        line.strip()))
+                sys.stdout.flush()
 
-                    print("{} {}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                          line.strip()))
-                    sys.stdout.flush()
-    time.sleep(600)
