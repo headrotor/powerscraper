41b3b0b1a19f9daaf7413fdcbc9be9623862e95b
headrotor <rrmutt@gmail.com>
2021-05-20 15:26:15 -0700

    checkpoint

--- demo.py
+++ demo.py
@@ -1,8 +1,32 @@
 from __future__ import print_function
 from __future__ import division
+import RPi.GPIO as GPIO
+import time
+
+
 
 fname = "scraperlog"
 
+
+RELAY = 18 # BCM 18, GPIO.1 physical pin 12
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY,GPIO.OUT)
+
+try:
+     while True:
+
+          GPIO.output(RELAY,True)
+          print("high")
+          time.sleep(1.0)
+          GPIO.output(RELAY,False)
+          print("low")
+          time.sleep(1.0)
+except KeyboardInterrupt:
+
+     GPIO.cleanup()               # clean up after yourself
+     print("interrupted")
+     exit(0)
+    
 with open(fname,'r') as fh:
      all_lines = fh.readlines()
 
