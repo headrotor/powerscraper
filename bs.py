1e6cadb5e5d518a2a37fa7200a0658997b2040f3
j t f <rrmutt@gmail.com>
2021-05-21 16:16:56 -0700

    checkpoint

--- bs.py
+++ bs.py
@@ -14,6 +14,33 @@ soup = BeautifulSoup(html, "html.parser")
 
 text = soup.get_text()
 
+table = soup.find("table", { "class" : "table-striped" })
+
+data = []
+#table_body = table.find('tbody')
+
+rows = table.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    eldata = [ele for ele in cols if ele]
+    if len(eldata) == 3:
+        data.append(eldata)
+
+print(len(data))
+for d in data:
+    print("{}, ".format(d[0]),end="")
+print(" counties")
+
+for d in data:
+    print("{}, ".format(d[2]),end="")
+
+print(" outages")
+#for d in data:
+#    print("{}, ".format(str(d[2])))
+
+#print(data)
+
 for line in text.split("\n"):
     if len(line.strip()) > 0:
         words = line.split()
