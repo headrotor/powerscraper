675461acaee2269facc093ab5cabd9adef4ec7bb
j t f <rrmutt@gmail.com>
2021-05-21 12:31:04 -0700

    checkpoint

--- canned-demo.py
+++ canned-demo.py
@@ -1,24 +1,40 @@
 from __future__ import print_function
 from __future__ import division
-import RPi.GPIO as GPIO
 import time
+import sys
 
+use_hw = True
+if len(sys.argv) > 1:
+     use_hw = False
+  
+if use_hw:
+     try:
+          import RPi.GPIO as GPIO
+     except ImportError:
+          print("Needs to be run on a Raspberry Pi to use the hardware.")
+          print("Run again with --dry_run to avoid this error")
+          exit(1)
+     RELAY = 18 # BCM 18, GPIO.1 physical pin 12
+     # Relay is connected NORMALLY CLOSED so gpio.cleanup() leaves it SET.
+     # set RELAY TRUE to TURN OFF
+     GPIO.setmode(GPIO.BCM)
+     GPIO.setup(RELAY,GPIO.OUT)
 
-
-
-RELAY = 18 # BCM 18, GPIO.1 physical pin 12
-# Relay is connected NORMALLY CLOSED so gpio.cleanup() leaves it SET.
-# set RELAY TRUE to TURN OFF
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY,GPIO.OUT)
-
+# if this file does not exist, "gunzip scraperlog.gz"
 fname = "scraperlog"
-threshold = 1000
+# turn off power if threshold is above this number
+threshold = 200
+# wait this many seconds before getting next line of data
+wait_seconds = 1.0
 
 
 
-# power on on startup
-GPIO.output(RELAY,False)
+print("Texas outage data scraped from https://poweroutage.us/area/utility/380")
+
+
+if use_hw:
+     # power on on startup
+     GPIO.output(RELAY,False)
 
 # read in file of scraped outage data    
 with open(fname,'r') as fh:
@@ -31,21 +47,27 @@ try:
           try:
                outages = int(fields[5].replace(',',""))
           except ValueError:
-               print("couyld not convert outage value")
-          print("{} {}".format(time_str, outages))
+               print("could not convert outage value")
+          print("{} customer outages: {}".format(time_str, outages))
           if outages > threshold:
-               print("above threshold")
-               GPIO.output(RELAY,True)
+               print("above threshold: POWER OFF")
+               if use_hw:
+                    GPIO.output(RELAY,True)
           else:
-               print("below threshold")
-               GPIO.output(RELAY,False)
-          if i > 10:
-               break
-          time.sleep(1.0)
+               print("below outage threshold of {}".format(threshold))
+               if use_hw:
+                    GPIO.output(RELAY,False)
+          # for testing, uncomment if you don't want to read the whole file
+          #if i > 10:
+          #     break
+
+          # wait and get the next line of data
+          time.sleep(wait_seconds)
 
 except KeyboardInterrupt:
      print("interrupted")
 
-GPIO.cleanup()               # clean up after yourself
+if use_hw:
+     GPIO.cleanup()               # clean up after yourself
 exit(0)
 
